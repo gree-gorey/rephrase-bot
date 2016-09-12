9dddd36fdf86953079567790a2ead2ac8837a0bb
gree-gorey <ignatyeff.g@yandex.ru>
2016-09-12 23:16:09 +0300

    add py

--- config.js
+++
@@ -1,3 +0,0 @@
-var OcpApimSubscriptionKey = "0875032496aa472699edb4dc08d1f023";
-
-exports.OcpApimSubscriptionKey = OcpApimSubscriptionKey;
---
+++ fetch.py
@@ -0,0 +1,60 @@
+import json
+import time
+import codecs
+import requests
+from config import OcpApimSubscriptionKey
+
+data = {
+    "language": "en",
+    "analyzerIds": [
+        "08ea174b-bfdb-4e64-987e-602f85da7f72",
+        "4fa79af1-f22c-408d-98bb-b7d7aeef7f04"],
+    "text": "You are"
+}
+
+headers = {
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': OcpApimSubscriptionKey
+}
+
+
+def create_dict():
+    d = dict()
+
+    errors = 0
+
+    with open('./hobbit.txt', 'r') as f:
+        paragraphs = f.read().split('\n\n')
+
+    for paragraph in paragraphs:
+        data["text"] = paragraph
+
+        r = requests.post('https://api.projectoxford.ai/linguistics/v1.0/analyze', data=json.dumps(data),
+                          headers=headers)
+
+        result = json.loads(r.text)
+
+        try:
+            tokens = result[0]['result'][0]['Tokens']
+            pos_tags = result[1]['result'][0]
+
+            for i, token in enumerate(tokens):
+                this_token = token['RawToken']
+                this_pos_tag = pos_tags[i]
+
+                if this_pos_tag not in d:
+                    d[this_pos_tag] = [this_token]
+                else:
+                    if this_token not in d[this_pos_tag]:
+                        d[this_pos_tag].append(this_token)
+
+        except:
+            errors += 1
+            print 'Exception {}'.format(errors)
+
+        time.sleep(1.1)
+
+    with codecs.open('./dict.json', 'w', 'utf-8') as w:
+        json.dump(d, w, ensure_ascii=False, indent=2)
+
+create_dict()
